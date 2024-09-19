7960f7d549c4d2e98b95044542b3cc6a4d22e070
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-19 14:15:58 +0600

    login done

--- js/login.js
+++ js/login.js
@@ -1,10 +1,22 @@
 // console.log("I am from login js")
 
-document.getElementById('btnLogin').addEventListener('click', function(event){
-    event.preventDefault(); // stop form refreshing 
-    console.log('login button clicked')
+document.getElementById("btnLogin").addEventListener("click", function (event) {
+  event.preventDefault(); // stop form refreshing
 
+  const phoneNumberField = document.getElementById("phoneNumber");
+  const phoneNumber = phoneNumberField.value;
+  const pinNumberField = document.getElementById("pinNumber");
+  const pinNumber = pinNumberField.value;
+  console.log(phoneNumber, pinNumber);
 
-    const phoneNumberField = document.getElementById('phoneNumber');
-    console.log(phoneNumberField.value)
-})
+  // this is temporary
+  if (phoneNumber === "01817482739" && pinNumber === "12345") {
+    console.log("you are logged in");
+    window.location.href = "/home.html";
+  } else {
+    alert("Please provide a valid Phone NUmber and Pin");
+  }
+
+  phoneNumberField.value = "";
+  pinNumberField.value = "";
+});
